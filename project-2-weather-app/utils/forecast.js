d4832c7a5a529fb1efc439d33fe4a78c218c18f7
Barnaby Thieme <bathieme@hotmail.com>
2021-02-04 16:06:43 +0100

    Add env var support.

--- project-2-weather-app/utils/forecast.js
+++ project-2-weather-app/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=dccad17973a5342519befc17d2fe8e8c&units=f&query="
+    const url = "http://api.weatherstack.com/current?access_key=" + WEATHERSTACK_API_KEY + "&units=f&query="
         + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude);
     request({url, json: true}, (error, { body }) => {
         if (error) {
